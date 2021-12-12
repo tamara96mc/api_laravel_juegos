c60dd39e64b380a84fad8f5de7c03e94e017f266
tamara96mc <tamara.96mc@gmail.com>
2021-12-12 12:11:28 +0100

    crear el proyecto de laravel

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Routing\UrlGenerator;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        if (env('REDIRECT_HTTPS')) {
+            $this->app['request']->server->set('HTTPS', true);
+        }
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    
+    public function boot(UrlGenerator $url)
+    {
+       if (env('REDIRECT_HTTPS')) {
+           $url->formatScheme('https://');
+       }
+    }
+}
